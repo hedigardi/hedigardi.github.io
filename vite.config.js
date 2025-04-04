7bcf48dea566205961289ed3b6c3bf3518060bae
hedigardi0 <hedigardi0@gmail.com>
2025-04-04 12:28:51 +0000

    updated-vite

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    mimeTypes: {
-      'text/jsx': ['jsx'],
-    },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+    exclude: [],
   },
   plugins: [react()],
 })
