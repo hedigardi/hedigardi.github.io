c2bc8f619b1452e5cb5f18d1c6cd82e2d5f122d1
Hedi Gardi <hedi.gardi@verified.eu>
2025-04-04 13:59:07 +0200

    initial-code

---
+++ src/Schemas/Data.js
@@ -0,0 +1,148 @@
+export const Data = {
+  profile: {
+    name: 'Hedi Gardi',
+    ocupation: 'Blockchain Developer',
+    location: 'Oslo, Norway',
+    email: 'hedigardi@hotmail.com',
+    telephone: '+4740314401',
+    image: 'images/me.jpg',
+  },
+  aboutMe: {
+    label: 'About',
+    description:
+      'I am a flexible and goal-oriented team player, driven by a passion for learning and precision. My primary focus lies in delivering excellent service while smoothly navigating new challenges. Endured by a strong enthusiasm for technology, my aim is to contribute to an environment that develops both creativity and determination.',
+  },
+  skills: {
+    technicalLabel: 'Skills',
+    softLabel: 'Additional',
+    technicalSkills: [
+      'Blochchain',
+      'Web3',
+      'React',
+      'JavaScript',
+      'TypeScript',
+      'Node.js',
+      'HTML5',
+      'CSS3',
+      'Git',
+      'GitHub',
+      'Full-Stack Development',
+      'Business and Law',
+      'Express.js',
+      'SASS',
+      'API',
+    ],
+    softSkills: [
+      'English (fluent)',
+      'Swedish (fluent)',
+      'Kurdish (native)',
+      'Norwegian (good)',
+      'Farsi (good)',
+      'Duch (basic)',
+      'Driving license (B)',
+    ],
+  },
+  socialMedia: {
+    label: 'SOCIAL',
+    social: [
+      {
+        label: 'Visit the profile of Hedi Gardi on LinkedIn',
+        name: 'linkedin',
+        url: 'https://www.linkedin.com/in/hedigardi/',
+        className: 'bxl-linkedin-square',
+      },
+      {
+        label: 'Visit the profile of Hedi Gardi on GitHub',
+        name: 'github',
+        url: 'https://github.com/hedigardi',
+        className: 'bxl-github',
+      },
+    ],
+  },
+  experience: {
+    works: [
+      {
+        title: 'Customer Support Agent',
+        period: 'Feb. 2024 - Present',
+        company: 'Verified',
+        description: [
+          "As a Customer Support Agent at Verified, I handle support cases, including operational support and technical assistance via phone and email. I maintain and develop Verified's strong customer base and collaborate across departments to ensure the best customer experience. My responsibilities also include training and guiding customers on our broad range of services. I work proactively with new technology, maintaining structure and punctuality. My role gives me the opportunity to be a key contributor in a rapidly growing market, with excellent opportunities for professional and personal development.",
+        ],
+      },
+      {
+        title: 'Junior Cloud Technician',
+        period: 'Jan. 2021 - Feb. 2024',
+        company: 'K3 Nordic AB',
+        description: [
+          'As a Junior Cloud Technician, I work with both internal and external IT support for various companies. My focus is on Microsoft services, but I also frequently engage in different projects, both big and small, where I assist with various types of migrations. Additionally, I assist with the popular scenario of transitioning from on-premises to the cloud.',
+        ],
+      },
+      {
+        title:
+          'Customer Service Representative/Education & System Administrator/Subject Matter Expert',
+        period: 'Oct. 2018 - Jan. 2021',
+        company: 'Ragn-Sells',
+        description: [
+          "I started working as a customer service representative at this environmentally conscious company. I handled incoming calls and emails from customers across the country, assisting both large clients such as NCC and PEAB, as well as small customers. The majority of the inquiries involved the emptying of customers' containers, such as containers or bins, among other tasks related to invoices and complaints.",
+          'After five months, I transitioned to a new role as a Training and System Coordinator for the customer service department, where I trained both new and existing employees. Additionally, I maintained various systems used within the company. I also updated instructions and guides and ensured that the customer service website, known as the "customer service portal," was up to date with all the necessary instructions and documents.',
+          'After a while, I was involved in a project to develop a new business system for the company, which will meant to be implemented in Sweden, Norway, Denmark, and Estonia.',
+        ],
+      },
+      {
+        title: 'Customer Service Representative',
+        period: 'Feb. 2017 - May. 2018',
+        company: 'Transcom (Tele2)',
+        description: [
+          "As a customer service representative, I handled incoming calls for Tele2. I consistently aimed to achieve set goals such as call duration, customer satisfaction, and upselling. My job involved providing effective solutions to customers' issues, whether it was changing a subscription, explaining a bill, or assisting with the purchase of a new phone/router. I conducted thorough needs analyses to offer personalized solutions to customers.",
+        ],
+      },
+      {
+        title: 'Client Advisor',
+        period: 'Sep. 2016 - Jan. 2017',
+        company: 'Louis Vuitton',
+        description: [
+          'As a client advisor at this luxury company, I was responsible for serving customers from the Nordic countries, the United Kingdom, South Africa, and Ireland. I ensured that each customer was treated according to the Louis Vuitton promise. I strived to understand customers and their needs and provided them with guidance in the best possible way.',
+        ],
+      },
+      {
+        title: 'Customer Service Representative',
+        period: 'Jan. 2015 - Aug. 2016',
+        company: 'U.S. Bank (Elavon)',
+        description: [
+          'As a customer support representative at one of the leading American banks, US Bank, I worked with customers to provide services such as invoice auditing, accounting updates, setting up new accounts, monitoring customer flows, resolving technical IT-related issues, and more. I maintained daily communication with customers via phone and used email as another means of communication for daily work. Additionally, I had face-to-face meetings to establish close relationships with customers and discuss important matters and improvements for the future.',
+        ],
+      },
+      {
+        title: 'Administrative Assistant',
+        period: 'Sep. 2014 - Jan. 2015',
+        company: 'Procter & Gamble',
+        description: [
+          "At this major American company, I had several administrative tasks and responsibilities within the company's 'Internal Records' archive department. I prepared laboratory notes and other project materials for specific scanning processes conducted in Germany. Additionally, I was responsible for inputting all the scanned notes into the company's database.",
+        ],
+      },
+      {
+        title: 'Administrator',
+        period: 'Jan. 2011 - Sep. 2013',
+        company: 'Posten Scanning (Aditro Logistics)',
+        description: [
+          'As an administrator/registrar, I worked in various departments (Giro, Invoice, Forms), where I sorted, reviewed, registered, and verified various types of paperwork, such as invoices, authorizations, bills, salaries, etc. In addition to my customer service duties, I maintained daily contact with customers and ensured that they received their paper materials on time.',
+        ],
+      },
+    ],
+    academic: [
+      {
+        career: 'Blockchain Developer',
+        date: '2025',
+        institution: 'Medieinstitutet, Stockholm',
+      },
+    ],
+    proyects: [
+      /*{
+        name: 'name',
+        company: 'company',
+        period: 'Jan. 2019 - Dec. 2020',
+        description: ['description'],
+      },*/
+    ],
+  },
+};
