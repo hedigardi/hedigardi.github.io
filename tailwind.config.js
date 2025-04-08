1e20725657f69147723a281f410f51aff1ae06cd
hedigardi0 <hedigardi0@gmail.com>
2025-04-08 11:14:15 +0000

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}",
-  ],
+ export default {
+  content: ["./src/**/*.{html,js,jsx}"],
   darkMode: 'class',
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
 
